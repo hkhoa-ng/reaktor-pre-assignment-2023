e8d931a5fb45ce18876ad7326decdf782426f30c
Khoa Nguyen <nhkhoa020400@gmail.com>
2022-12-15 13:02:44 +0000

    Update package.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
-import {Box, Center, Text, Heading, VStack, Button} from "@chakra-ui/react";
+import { Box, Center, Text, Heading, VStack, Button } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
+import axios from "axios";
+
 function App() {
-
-  // fetch('https://assignments.reaktor.com/birdnest/drones', {
-  //   method: 'GET',
-  //   mode: 'no-cors',
-  // })
-  //   .then(res => res.text())
-  //   .then(data => {
-  //     const parser = new DOMParser();
-  //     const xml = parser.parseFromString(data, 'application/xml')
-  //     console.log(xml);
-  //   })
-  //   .catch(console.error);
-
- 
-  // fetch('https://catfact.ninja/fact')
-  //   .then(res => res.text())
-  //   .then(data => {
-  //     console.log(data)
-  //   });
-
-
-
   const [getData, setGetData] = useState(true);
-  const fetchDroneData = () => {
-    fetch('https://catfact.ninja/fact', {
-      method: 'GET',
-      mode: 'no-cors',
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
+
+  const dronesAPI = "https://assignments.reaktor.com/birdnest/drones";
+  const serialNumber = "SN-zdW3p9ZfiT";
+
+  const getDroneData = () => {
+    axios
+      .get(dronesAPI, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-  }
+  };
+
+  const getPilotData = async (serialNumber) => {
+    const url = `https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`;
+    fetch(url, {
+      method: "GET",
+      mode: "no-cors",
+    })
+      .then((res) => res.text())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
   useEffect(() => {
-    fetchDroneData();
-  }, [])
-
+    getDroneData();
+  }, [getData]);
 
   return (
-    <Box w="100vw" h="100vh" >
+    <Box w="100vw" h="100vh">
       <Center>
         <VStack>
           <Heading>Birdnest</Heading>
-          <Button onClick={() => setGetData((prev) => !prev)}>Fetch Data</Button>
+          <Button onClick={() => setGetData((prev) => !prev)}>
+            Fetch Data
+          </Button>
         </VStack>
       </Center>
     </Box>
